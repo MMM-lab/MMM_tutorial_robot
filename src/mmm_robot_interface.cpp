f6315581891c05cf8b369a469f1ba5dca4f551af
RyosukeMatsushima <m.ryosuke@me.com>
2020-07-11 23:34:31 +0900

    add Torque_Control good gain all 0.01

--- src/mmm_robot_interface.cpp
+++ src/mmm_robot_interface.cpp
@@ -1,18 +1,26 @@
 #include "ros/ros.h"
 #include "std_msgs/Float64.h"
 #include "sensor_msgs/JointState.h"
+#include "../include/torque_calculater.hpp"
 
 #include <string>
 
-void subscribeState(const sensor_msgs::JointState rrobotState)
+Robot_Wheel_Rate robot_wheel_rate;
+
+void subscribeState(const sensor_msgs::JointState robotState)
 {
-  ROS_INFO("joint1 position %f", rrobotState.position[0]);
-  ROS_INFO("joint2 position %f", rrobotState.position[1]);
+  robot_wheel_rate.left = robotState.velocity[0];
+  robot_wheel_rate.right = robotState.velocity[1];
+  ROS_INFO("joint1 velocity %f", robot_wheel_rate.left);
+  ROS_INFO("joint2 velocity %f", robot_wheel_rate.right);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "mmm_robot_interface");
+
+  Torque_Control torque_control;
+  torque_control.set_param( 0.2, 0.1, std::stof(argv[3]), std::stof(argv[4]), std::stof(argv[5]));
 
   ros::NodeHandle n;
 
@@ -26,14 +34,23 @@ int main(int argc, char **argv)
   int count = 0;
   while (ros::ok())
   {
+    Robot_Input robot_input;
+    Robot_State target_robot_state;
+
+    target_robot_state.yow_rate = std::stof(argv[1]);
+    target_robot_state.velocity = std::stof(argv[2]);
+
+
+    torque_control.get_torques(&robot_input, &robot_wheel_rate, &target_robot_state);
+
     std_msgs::Float64 joint1_torque;
     std_msgs::Float64 joint2_torque;
 
-    joint1_torque.data = std::stof(argv[1]);
-    joint2_torque.data = std::stof(argv[2]);
+    joint1_torque.data = robot_input.left;
+    joint2_torque.data = robot_input.right;
 
-    // ROS_INFO("%f", joint1_torque.data);
-    // ROS_INFO("%f", joint2_torque.data);
+    ROS_INFO("joint1_torque %f", joint1_torque.data);
+    ROS_INFO("joint2_torque %f", joint2_torque.data);
 
     joint1_pub.publish(joint1_torque);
     joint2_pub.publish(joint2_torque);
